763607b66722bec26766282b2ada52d0fc42763b
Zander Gordan <me@zandergordan.com>
2020-12-17 17:10:36 -0500

    Enforce styleguide adherence w/ `lintr`

--- programs/MRIP.catch.R
+++ programs/MRIP.catch.R
@@ -1,14 +1,23 @@
 library(survey)
 
-MRIP_catch <- function(intdir = NULL, common = NULL, st = NULL, styr = NULL, endyr = NULL, dom = NULL) {
+calculate_catch <- function(
+  intdir = NULL,
+  common = NULL,
+  st = NULL,
+  styr = NULL,
+  endyr = NULL,
+  dom = NULL
+) {
   if (is.null(intdir)) stop("Need main directory location of intercept files.")
   if (is.null(st)) stop("No state code was specified.")
   if (is.null(styr)) stop("Starting year is missing.")
   if (is.null(endyr)) stop("Ending year is missing.")
-  # If species equal NULL all species are used.
+  # If species equal NULL all species are used
   if (length(grep("/", intdir)) == 1) {
-    din <- ifelse(substr(intdir, nchar(intdir), nchar(intdir)) %in% "/",
-      paste0(intdir, "int"), paste0(intdir, "/int")
+    din <- ifelse(
+      substr(intdir, nchar(intdir), nchar(intdir)) %in% "/",
+      paste0(intdir, "int"),
+      paste0(intdir, "/int")
     )
   }
   if (length(grep("\\\\", intdir)) == 1) {
@@ -22,23 +31,22 @@ MRIP_catch <- function(intdir = NULL, common = NULL, st = NULL, styr = NULL, end
   styr <- as.character(styr)
   endyr <- as.character(endyr)
   wave <- as.character(c(1, 2, 3, 4, 5, 6))
-  dom_id <- NULL
   temp <- NULL
   temp1 <- NULL
   rbind2 <- function(input1, input2) {
     if (!is.null(ncol(input1))) {
-      n.input1 <- ncol(input1)
-      n.input2 <- ncol(input2)
+      n_input1 <- ncol(input1)
+      n_input2 <- ncol(input2)
 
-      if (n.input2 < n.input1) {
-        TF.names <- which(names(input2) %in% names(input1))
-        column.names <- names(input2[, TF.names])
+      if (n_input2 < n_input1) {
+        tf_names <- which(names(input2) %in% names(input1))
+        column_names <- names(input2[, tf_names])
       }
       else {
-        TF.names <- which(names(input1) %in% names(input2))
-        column.names <- names(input1[, TF.names])
+        tf_names <- which(names(input1) %in% names(input2))
+        column_names <- names(input1[, tf_names])
       }
-      return(rbind(input1[, column.names], input2[, column.names]))
+      return(rbind(input1[, column_names], input2[, column_names]))
     }
     if (is.null(ncol(input1))) {
       return(rbind(input1, input2))
@@ -49,15 +57,19 @@ MRIP_catch <- function(intdir = NULL, common = NULL, st = NULL, styr = NULL, end
     for (j in 1:as.numeric(length(wave))) {
       # Get catch
       wv <- wave[j]
-      t3 <- read.csv(paste0(din, yr, "/", "catch_", yr, wv, ".csv"),
-        colClasses = "character", na.strings = "../../../../Downloads/MRIP Catch Estimation Program Template in R"
+      t3 <- read.csv(
+        paste0(din, yr, "/", "catch_", yr, wv, ".csv"),
+        colClasses = "character",
+        na.strings = "../../../../Downloads/MRIP Catch Estimation Program Template in R"
       )
       t3 <- t3[t3$ST %in% st, ]
       names(t3) <- tolower(names(t3))
       temp <- rbind2(temp, t3)
-      # get trips
-      t4 <- read.csv(paste0(din, yr, "/", "trip_", yr, wv, ".csv"),
-        colClasses = "character", na.strings = "../../../../Downloads/MRIP Catch Estimation Program Template in R"
+      # Get trips
+      t4 <- read.csv(
+        paste0(din, yr, "/", "trip_", yr, wv, ".csv"),
+        colClasses = "character",
+        na.strings = "../../../../Downloads/MRIP Catch Estimation Program Template in R"
       )
       t4 <- t4[t4$ST %in% st, ]
       names(t4) <- tolower(names(t4))
@@ -65,21 +77,28 @@ MRIP_catch <- function(intdir = NULL, common = NULL, st = NULL, styr = NULL, end
     }
   }
   convtolow <- function(x) {
-    for (i in 1:ncol(x)) x[, i] <- tolower(x[, i])
+    for (i in seq_len(ncol(x))) {
+      x[, i] <- tolower(x[, i])
+    }
     return(x)
   }
   temp <- convtolow(temp)
   temp1 <- convtolow(temp1)
   temp <- temp[, c(
     "common", "strat_id", "psu_id", "st", "id_code", "sp_code",
-    "claim", "release", "harvest", "tot_len_a", "wgt_a", "tot_len_b1", "wgt_b1", "fl_reg", "tot_cat",
+    "claim", "release", "harvest",
+    "tot_len_a", "wgt_a",
+    "tot_len_b1", "wgt_b1",
+    "fl_reg", "tot_cat",
     "wgt_ab1", "tot_len", "landing"
   )]
   temp <- temp[order(temp$strat_id, temp$psu_id, temp$id_code), ]
   temp1 <- temp1[order(temp1$strat_id, temp1$psu_id, temp1$id_code), ]
   dataset <- merge(temp1, temp,
-    by.x = c("strat_id", "psu_id", "id_code", "st"),
-    by.y = c("strat_id", "psu_id", "id_code", "st"), all.x = FALSE, all.y = FALSE
+                   by.x = c("strat_id", "psu_id", "id_code", "st"),
+                   by.y = c("strat_id", "psu_id", "id_code", "st"),
+                   all.x = FALSE,
+                   all.y = FALSE
   )
   dataset$common <- as.character(dataset$common)
   dataset$common <- ifelse(is.na(dataset$common), "", dataset$common)
@@ -91,21 +110,29 @@ MRIP_catch <- function(intdir = NULL, common = NULL, st = NULL, styr = NULL, end
   mainlev <- length(dom)
   if (length(dom) > 0) {
     for (l in 1:mainlev) {
-      if (!any(names(dom)[l] == names(dataset))) stop(paste("Variable ", names(dom[l]), "not found in MRIP dataset"))
+      if (!any(names(dom)[l] == names(dataset))) {
+        stop(paste("Variable ", names(dom[l]), "not found in MRIP dataset"))
+      }
       if (any(names(dom)[l] == names(dataset))) {
         dataset[, ncol(dataset) + 1] <- "DELETE"
         names(dataset)[ncol(dataset)] <- paste0(names(dom)[l], "1")
         colpos <- which(names(dataset) == names(dom[l]))
         sublev <- length(dom[[l]])
         for (k in 1:sublev) {
-          dataset[, ncol(dataset)] <- ifelse(dataset[, colpos] %in% as.character(dom[[l]][[k]]), paste0(names(dom)[l], k), dataset[, ncol(dataset)])
+          dataset[, ncol(dataset)] <- ifelse(
+            dataset[, colpos] %in% as.character(dom[[l]][[k]]),
+            paste0(names(dom)[l], k),
+            dataset[, ncol(dataset)]
+          )
         }
         dom_ids <- c(dom_ids, names(dom[l]))
       }
     }
     test <- c("year", "wave", "st", "sub_reg", "mode_fx", "area_x")
     for (gg in 1:as.numeric(length(dom_ids))) {
-      if (!any(dom_ids[gg] == test)) test[as.numeric(length(test) + 1)] <- paste0(dom_ids[gg], "1")
+      if (!any(dom_ids[gg] == test)) {
+        test[as.numeric(length(test) + 1)] <- paste0(dom_ids[gg], "1")
+      }
       if (any(dom_ids[gg] == test)) {
         colpos <- which(test == dom_ids[gg])
         test[colpos] <- paste0(dom_ids[gg], "1")
@@ -115,7 +142,15 @@ MRIP_catch <- function(intdir = NULL, common = NULL, st = NULL, styr = NULL, end
     ## add Non-domain names to values in columns
     for (gg in 1:as.numeric(length(test))) {
       if (substr(test[gg], nchar(test[gg]), nchar(test[gg])) != "1") {
-        eval(parse(text = paste0("dataset$", test[gg], '<-paste("', as.character(test[gg]), '",dataset$', test[gg], ',sep="")')))
+        eval(parse(text = paste0(
+          "dataset$",
+          test[gg],
+          '<-paste("',
+          as.character(test[gg]),
+          '",dataset$',
+          test[gg],
+          ',sep="")'
+        )))
       }
     }
     for (hh in 1:as.numeric(length(test))) {
@@ -123,7 +158,7 @@ MRIP_catch <- function(intdir = NULL, common = NULL, st = NULL, styr = NULL, end
       if (hh > 1) texter <- paste0(texter, ",", "dataset$", test[hh])
     }
     eval(parse(text = paste0("dataset$dom_id<-c(paste(", texter, ",sep=''))")))
-  } # dom>0
+  }
 
   if (length(dom) == 0) {
     dataset$year <- paste0("year", dataset$year)
@@ -132,7 +167,10 @@ MRIP_catch <- function(intdir = NULL, common = NULL, st = NULL, styr = NULL, end
     dataset$sub_reg <- paste0("sub_reg", dataset$sub_reg)
     dataset$mode_fx <- paste0("mode_fx", dataset$mode_fx)
     dataset$area_x <- paste0("area_x", dataset$area_x)
-    dataset$dom_id <- paste0(dataset$year, dataset$wave, dataset$st, dataset$sub_reg, dataset$mode_fx, dataset$area_x)
+    dataset$dom_id <- paste0(dataset$year, dataset$wave,
+                             dataset$st, dataset$sub_reg,
+                             dataset$mode_fx, dataset$area_x
+    )
   }
   dataset$tot_cat <- as.numeric(dataset$tot_cat)
   dataset$landing <- as.numeric(dataset$landing)
@@ -168,15 +206,20 @@ MRIP_catch <- function(intdir = NULL, common = NULL, st = NULL, styr = NULL, end
       weights = ~wp_int, nest = TRUE, data = dataset1
     )
     options(survey.lonely.psu = "certainty")
-    results <- svyby(~ total.catch + harvest.A.B1 + claim.A + reported.B1 + released.B2 + weight,
-      ~dom_id, dfpc, svytotal,
-      vartype = c("se", "cv"), keep.names = FALSE
+    results <- svyby(
+      ~ total.catch + harvest.A.B1 + claim.A + reported.B1 + released.B2 + weight,
+      ~dom_id,
+      dfpc,
+      svytotal,
+      vartype = c("se", "cv"),
+      keep.names = FALSE
     )
     names(results) <- c(
       "Domain", "total.catch", "harvest.A.B1", "claim.A",
-      "reported.B1", "released.B2", "weight", "SE.total.catch", "SE.harvest.A.B1",
-      "SE.claim.A", "SE.reported.B1", "SE.released.B2", "SE.weight", "PSE.total.catch",
-      "PSE.harvest.A.B1", "PSE.claim.A", "PSE.reported.B1", "PSE.released.B2", "PSE.weight"
+      "reported.B1", "released.B2", "weight", "SE.total.catch",
+      "SE.harvest.A.B1", "SE.claim.A", "SE.reported.B1", "SE.released.B2",
+      "SE.weight", "PSE.total.catch", "PSE.harvest.A.B1", "PSE.claim.A",
+      "PSE.reported.B1", "PSE.released.B2", "PSE.weight"
     )
     if (length(grep("DELETE", results$Domain, fixed = TRUE)) > 0) {
       results <- results[-grep("DELETE", results$Domain, fixed = TRUE), ]
@@ -196,9 +239,10 @@ MRIP_catch <- function(intdir = NULL, common = NULL, st = NULL, styr = NULL, end
     )
     names(results) <- c(
       "Domain", "total.catch", "harvest.A.B1", "claim.A",
-      "reported.B1", "released.B2", "weight", "SE.total.catch", "SE.harvest.A.B1",
-      "SE.claim.A", "SE.reported.B1", "SE.released.B2", "SE.weight", "PSE.total.catch",
-      "PSE.harvest.A.B1", "PSE.claim.A", "PSE.reported.B1", "PSE.released.B2", "PSE.weight"
+      "reported.B1", "released.B2", "weight", "SE.total.catch",
+      "SE.harvest.A.B1", "SE.claim.A", "SE.reported.B1", "SE.released.B2",
+      "SE.weight", "PSE.total.catch", "PSE.harvest.A.B1", "PSE.claim.A",
+      "PSE.reported.B1", "PSE.released.B2", "PSE.weight"
     )
     if (length(grep("DELETE", results$Domain, fixed = TRUE)) > 0) {
       results <- results[-grep("DELETE", results$Domain, fixed = TRUE), ]
